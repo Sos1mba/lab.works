b1c6376b8d8e59c3e75d061cb1f197ba31867a21
Vlad_Deineko <vladd1904@gmail.com>
2026-09-21 15:49:42 +0300

    task

---
+++ 1/task.59.py
@@ -0,0 +1,11 @@
+
+
+
+
+a1 = input("")
+
+b1 = sum(map(int, a1))
+
+
+print(b1)
+
---
+++ 1/task.73.py
@@ -0,0 +1,7 @@
+a1 = int(input("Number "))
+a2 = int(input("Number "))
+a3 = int(input("Number "))
+
+print(f"{hex(a1)[2:]}")
+print(f"{hex(a2)[2:]}")
+print(f"{hex(a3)[2:]}")
